6358cc27e02231ac70460b4d2cfef7ce168c55ea
Jairo Galvez <jgalvez@baufest.com>
2026-02-28 15:37:05 -0500

    docs: mejorando README

--- internal/domain/state/ports/state_manager.go
+++ internal/domain/state/ports/state_manager.go
@@ -4,16 +4,12 @@ import (
 	"github.com/jairoprogramador/vex/internal/domain/state/vos"
 )
 
-// StateManager define la interfaz para gestionar el estado de los pasos.
 type StateManager interface {
-	// HasStateChanged comprueba si el estado actual de un paso ha cambiado
-	// en comparación con el estado guardado, según una política de caché.
 	HasStateChanged(
 		stateTablePath string,
 		currentState vos.CurrentStateFingerprints,
 		policy vos.CachePolicy,
 	) (bool, error)
 
-	// UpdateState guarda el nuevo estado de un paso.
 	UpdateState(stateTablePath string, currentState vos.CurrentStateFingerprints) error
 }
